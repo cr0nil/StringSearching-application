411a7db320254ab3778ee808559986d5d64d132e
LuCkY <LuCkY@DESKTOP-A0S2S10>
2017-11-30 13:36:57 +0100

    Dodanie DialogsUtils

---
+++ src/application/dialogs/DialogsUtils.java
@@ -0,0 +1,20 @@
+
+package application.dialogs;
+
+import java.util.Optional;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+public class DialogsUtils {
+    
+    public static Optional<ButtonType> confirmationDialog() {
+        Alert potwierdz = new Alert(Alert.AlertType.CONFIRMATION);
+        potwierdz.setTitle("Wyjście");
+        potwierdz.setHeaderText("Czy na pewno chcesz wyjść??");
+        Optional<ButtonType> result = potwierdz.showAndWait();
+        return result;
+
+    }
+    
+    
+}
