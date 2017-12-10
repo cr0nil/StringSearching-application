0b4c97e6de817e069420927e8c23f3b5caf36181
LuCkY <LuCkY@DESKTOP-A0S2S10>
2017-12-10 17:36:10 +0100

    Refactor

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,14 +1,10 @@
 package application;
 
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 public class Main extends Application {
 	 public Stage primartStage2;
